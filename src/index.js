136e538f3a721ad64aae0cd0e92c145fac4b71b8
Bala Raman <srbala@gmail.com>
2020-01-24 21:48:28 -0500

    Update index.js
    Making use under IE11

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import 'whatwg-fetch'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
